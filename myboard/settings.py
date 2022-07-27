878075a5e7a394436e28a2d88ca8ad93a5ce1ff0
BaekChanwoo <bcw043785@gmail.com>
2022-07-27 16:04:35 +0900

    Commit message2

--- myboard/settings.py
+++ myboard/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'j#=lm3=941j&pytw*0c=$lg#!02m#&hilr$fv&t4@1fw&q!g!e'
 SECRET_KEY = os.environ['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
